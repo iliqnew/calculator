2f3f26e373b17cfd4c3ff89241d118727cabe85e
iliqnew <iliqanew@gmail.com>
2024-05-20 00:04:51 +0300

    add metrics

--- calculator_project/calculator/models.py
+++ calculator_project/calculator/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class OperationCount(models.Model):
+    addition_count = models.PositiveIntegerField(default=0)
+    subtraction_count = models.PositiveIntegerField(default=0)
+    total_expressions = models.PositiveIntegerField(default=0)
+
+    @property
+    def average_operations(self):
+        if self.total_expressions == 0:
+            return 0
+        return (self.addition_count + self.subtraction_count) / self.total_expressions
--- calculator_project/calculator/views.py
+++ calculator_project/calculator/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ExpressionSerializer
 from .services.parser import ExpressionParser, InvalidExpressionException
+from .models import OperationCount
 
 
 class CalculationView(APIView):
@@ -13,7 +14,28 @@ class CalculationView(APIView):
             parser = ExpressionParser()
             try:
                 result = parser.evaluate(expression)
-                return Response({"result": result}, status=status.HTTP_200_OK)
+
+                # Trackers
+                operation_count, _ = OperationCount.objects.get_or_create(id=1)
+                addition_count = expression.count("+")
+                subtraction_count = expression.count("-")
+
+                operation_count.addition_count += addition_count
+                operation_count.subtraction_count += subtraction_count
+                operation_count.total_expressions += 1
+                operation_count.save()
+
+                average_operations = operation_count.average_operations
+
+                return Response(
+                    {
+                        "result": result,
+                        "addition_count": addition_count,
+                        "subtraction_count": subtraction_count,
+                        "average_operations": average_operations,
+                    },
+                    status=status.HTTP_200_OK,
+                )
             except InvalidExpressionException as e:
                 return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
