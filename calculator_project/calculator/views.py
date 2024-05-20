db70cf911c4b61e1acf6d8e8c8217fbc49cb56a3
iliqnew <iliqanew@gmail.com>
2024-05-20 11:02:30 +0300

    time metrics

--- calculator_project/calculator/views.py
+++ calculator_project/calculator/views.py
@@ -1,3 +1,4 @@
+import time
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,13 +9,15 @@ from .models import OperationCount
 
 class CalculationView(APIView):
     def post(self, request):
+        request_process_time_start = time.time()
         serializer = ExpressionSerializer(data=request.data)
         if serializer.is_valid():
             expression = serializer.validated_data["expression"]
             parser = ExpressionParser()
             try:
+                evaluation_time_start = time.time()
                 result = parser.evaluate(expression)
-
+                evaluation_time = time.time() - evaluation_time_start
                 # Trackers
                 operation_count, _ = OperationCount.objects.get_or_create(id=1)
                 addition_count = expression.count("+")
@@ -33,6 +36,9 @@ class CalculationView(APIView):
                         "addition_count": addition_count,
                         "subtraction_count": subtraction_count,
                         "average_operations": average_operations,
+                        "evaluation_time": evaluation_time,
+                        "request_process_time": time.time()
+                        - request_process_time_start,
                     },
                     status=status.HTTP_200_OK,
                 )
